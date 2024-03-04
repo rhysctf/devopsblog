82dd053f387a5ead531b54bbc76adcefb2f33a56
rhysctf <rhys7homas@gmail.com>
2024-03-04 21:30:16 +0000

    build.sh correct syntax

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "building new docker image with image tag: $1"
-docker build . devopsblog-image:$1
-docker push devopsblog-image:$1
+docker build -t rhys7homas/devopsblog-image:$1 .
+docker push rhys7homas/devopsblog-image:$1
 echo "done"
