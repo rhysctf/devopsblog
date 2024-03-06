2b8707a428e8fabe0a9adfbc80efe7a98d76ce41
rhysctf <rhys7homas@gmail.com>
2024-03-06 02:25:39 +0000

    added gitgub releases

--- build.sh
+++ build.sh
@@ -5,6 +5,17 @@ git commit -m "$2"
 git tag -a $1 HEAD -m "$2"
 git push
 
+echo "Creating GitHub release"
+GH_TOKEN=$(cat ~/Downloads/github-token.txt)
+GH_REPO="rhysctf/devopsblog"
+GH_API="https://api.github.com/repos/$GH_REPO"
+TAG_NAME="$1"
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -H "Authorization: token $GH_TOKEN" \
+    -d "{\"tag_name\": \"$TAG_NAME\"}" \
+    "$GH_API/releases"
+
 echo "Building new Docker Image with Image Tag: $1"
 docker build -t rhys7homas/devopsblog-image:$1 .
 docker push rhys7homas/devopsblog-image:$1
